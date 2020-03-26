8c42186277346b529c45f77fd8ac2053292dc885
Hugo Gomes <hugogomes@MBP-de-Hugo-2.lan>
2020-03-26 20:11:40 +0000

    Updates

---
+++ precache-manifest.5323fa10c164550de98d4205c010a384.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c968b77efb7f182b74bfbc22a3b06287",
+    "url": "/multi-manage/index.html"
+  },
+  {
+    "revision": "3f93599681495eeacaa2",
+    "url": "/multi-manage/static/css/2.856cb68b.chunk.css"
+  },
+  {
+    "revision": "ef377eed4ee30a16fb45",
+    "url": "/multi-manage/static/css/main.f995ea40.chunk.css"
+  },
+  {
+    "revision": "3f93599681495eeacaa2",
+    "url": "/multi-manage/static/js/2.c173edb8.chunk.js"
+  },
+  {
+    "revision": "ca2529ec811b23fbaecc956862a512b2",
+    "url": "/multi-manage/static/js/2.c173edb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef377eed4ee30a16fb45",
+    "url": "/multi-manage/static/js/main.c3dcb7fd.chunk.js"
+  },
+  {
+    "revision": "dc4839802ee292f68843",
+    "url": "/multi-manage/static/js/runtime-main.df47cd8f.js"
+  },
+  {
+    "revision": "3adb47a569c976990e1bb1f4c942e58b",
+    "url": "/multi-manage/static/media/auth_manager.3adb47a5.jpg"
+  },
+  {
+    "revision": "66cfe637e307dad387b8a5bcd820f611",
+    "url": "/multi-manage/static/media/landing_page.66cfe637.png"
+  },
+  {
+    "revision": "bd9822d3af66708d164802573d1bb0fb",
+    "url": "/multi-manage/static/media/loading.bd9822d3.svg"
+  }
+]);
